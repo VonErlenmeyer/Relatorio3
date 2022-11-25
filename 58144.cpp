c7e3a95bcb5917c10b1f2121d41e0ada5123684c
VonErlenmeyer <ismaelture@gmail.com>
2022-11-25 19:54:09 +0000

    Just SOme Tests

--- 58144.cpp
+++ 58144.cpp
@@ -4,10 +4,25 @@ using namespace std;
 
 int main(void){
 
+double h=0.9;
+double y[50]={0};
+double x0 = 0;
+y[0]=1;
+int i;
+double k1, k2, k3, k4;
+fstream Test;
+Test.open("Zeus.dat", ios::out);
 
+for(i=0; i<50; i++){
+    k1 = 5*y[i];
+    k2 = 5*y[i]+k1*h/2;
+    k3 = 5*y[i]+k2*h/2;
+    k4 = 5*y[i]+k3*h;
 
-
-
+    y[i+1]=y[i]+(h/6)*(k1+1*k2+2*k3+k4);
+    x0 += h;
+    Test << x0 << "\t" << y[i] << endl;
+}
 
 return 0;
 }
